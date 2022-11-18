1285cc9313f4f86774345e854bf7fca0df11507f
JaretWright <jaret.wright@georgiancollege.ca>
2022-11-17 21:21:09 -0500

    Added images and logo

--- src/main/java/com/example/f22comp1011lhw9/Main.java
+++ src/main/java/com/example/f22comp1011lhw9/Main.java
@@ -3,6 +3,7 @@ package com.example.f22comp1011lhw9;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Movies R Us!");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/logo.png")));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/f22comp1011lhw9/SearchViewController.java
+++ src/main/java/com/example/f22comp1011lhw9/SearchViewController.java
@@ -1,13 +1,17 @@
 package com.example.f22comp1011lhw9;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class SearchViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchViewController implements Initializable {
 
     @FXML
     private ListView<Movie> listView;
@@ -24,4 +28,8 @@ public class SearchViewController {
     @FXML
     private VBox selectedVBox;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        resultsBox.setVisible(false);
+    }
 }
