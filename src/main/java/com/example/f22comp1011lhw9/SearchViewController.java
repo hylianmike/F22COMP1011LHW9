9077a3d1e4ea9f3e481bd03a956c053dbc831513
JaretWright <jaret.wright@georgiancollege.ca>
2022-11-17 21:01:53 -0500

    Updating the view-2

---
+++ src/main/java/com/example/f22comp1011lhw9/SearchViewController.java
@@ -0,0 +1,27 @@
+package com.example.f22comp1011lhw9;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class SearchViewController {
+
+    @FXML
+    private ListView<Movie> listView;
+
+    @FXML
+    private ImageView posterImageView;
+
+    @FXML
+    private HBox resultsBox;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private VBox selectedVBox;
+
+}
